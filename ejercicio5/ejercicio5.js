a6fe0656eeff6e6c19ca436e5da2cad24350d872
ZOMPO <alepeiro277@gmail.com>
2021-07-14 22:16:55 -0300

    se corrigieron los errores

--- ejercicio5/ejercicio5.js
+++ ejercicio5/ejercicio5.js
@@ -1,32 +1,22 @@
 /**
  * Mostrar todos los numeros divisores de un numero introduce
  * en prompt
- * 
- * 
+ *
+ *
  * */
- 
-const start = $("#start")
-start.on("click",function(){
- 
+const caja = $("#caja");
+const start = $("#start");
+start.on("click", function (e) {
+  var numero = parseInt(prompt("Introduce un numero", 1));
 
-var numero =  parseInt(prompt("Introduce un numero", 1));
+  var texto = "";
 
-for(var i = 1; i <= numero; i++){
-
-    if(numero%i == 0){
-
-        document.write("<p>Divisor: "+ i + "</p>")
+  for (var i = 1; i <= numero; i++) {
+    if (numero % i == 0) {
+      texto = texto + "<p>Divisor: " + i + "</p>";
     }
+  }
+  e.preventDefault();
+  caja.html(texto);
 
-
-
-
-
-}
-
-
-
-
-  
-})
-
+});
--- ejercicio6/ejercicio6.js
+++ ejercicio6/ejercicio6.js
@@ -2,33 +2,31 @@
  * Que nos diga si es par o impar.
  * 1. Ventana prompt
  * 2. Si no es valido que nos pida de nuevo el numero
- * 
+ *
  */
- 
-const start = $("#start")
-start.on("click",function(){
- 
 
-var numero =  parseInt(prompt("Introduce un numero", 0));
+const start = $("#start");
+start.on("click", function () {
+  var numero = parseInt(prompt("Introduce un numero", 0));
 
-while(isNaN(numero)){
-    
-    numero =  parseInt(prompt("Introduce un numero", 0));
-}
+  while (isNaN(numero)) {
+      alert ("no es un numero valido");
+    numero = parseInt(prompt("Introduce un numero", 0));
+  }
 
-alert((numero % 2 == 0) ? numero+" es numero par" : numero+" es numero impar" )
+  alert(
+    numero % 2 == 0 ? numero + " es numero par" : numero + " es numero impar"
+  );
+});
 
-  
-})
-/*
-      | El ejemplo de el es asi
-      |
-     \|/
+    //   | El ejemplo de el es asi
+    //   |
+    //  \|/
      
-if (numero % 2 == 0) {
-    alert("Es un numero par")
+// if (numero % 2 == 0) {
+//     alert("Es un numero par")
     
-} else {
-    alert("Es impar")
-}
-*/
+// } else {
+//     alert("Es impar")
+// }
+
